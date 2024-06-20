a05e8a31c9adedf8232af0d45a052a2b1bf04d1f
valento <valentin.mundrov@gmail.com>
2024-06-20 13:17:26 +0300

    swagger added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,7 +13,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   const port = process.env.PORT || 3000;
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Valento API')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup(globalPrefix, app, document)
+  
+  // Lift the Server
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
